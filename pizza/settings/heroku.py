b25fd914b9132ed20625ba5b1a40db4b119d5e83
Pythonsway <adam@pythonsway.it>
2019-03-12 22:42:43 +0100

    deploy ready

--- pizza/settings/heroku.py
+++ pizza/settings/heroku.py
@@ -7,6 +7,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['cs50w-proj3-pizza.herokuapp.com']
 
-# Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# # Heroku: Update database configuration from $DATABASE_URL.
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500)
+}
