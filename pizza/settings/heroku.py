0b245086bbcb1f75f3025d81f1bc9952bc32f4a8
Pythonsway <adam@pythonsway.it>
2019-12-17 22:18:50 +0100

    sendgrid integration

--- pizza/settings/heroku.py
+++ pizza/settings/heroku.py
@@ -1,9 +1,19 @@
+import os
+
 from .base import *
 
 import dj_database_url
 
 
 DEBUG = False
+
+# transactional emails
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = 'Pizza Portal <noreply@sendgrid.net>'
 
 ALLOWED_HOSTS = ['cs50w-proj3-pizza.herokuapp.com']
 
